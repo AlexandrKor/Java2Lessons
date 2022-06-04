4e50e197b12c867772b7e8c1ae17bbf080acf02d
Aleksandr <kornedj@gmail.com>
2022-06-04 13:47:56 +0300

    Выполнил второе задание для Домашнего задания №3

---
+++ src/HomeWork3/Phonebook.java
@@ -0,0 +1,12 @@
+package HomeWork3;
+
+import java.util.Set;
+
+public interface Phonebook {
+
+    void add(String surname, String phoneNumber);
+
+    Set<String> get(String surname);
+
+    Set<String> getAllSurname();
+}
